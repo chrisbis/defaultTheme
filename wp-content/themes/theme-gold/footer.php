1884988a4edd922f49397d2b3a4ff92f9179f3bb
Christoph <c.kleber@aol.com>
2020-07-24 12:50:50 +0200

    Refactor to theme-gold

---
+++ wp-content/themes/theme-gold/footer.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Theme_Gold
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="footer-menus">
+			<div class="container">
+				<div class="row">
+						<div class="col-md-2 footer-menu menu-left">
+							<?php
+							if ( is_front_page() ) :
+								wp_nav_menu(
+									array(
+										'theme_location' => 'header',
+										'menu_class'     => 'primary-menu',
+									)
+								);
+							else :
+								wp_nav_menu(
+									array(
+										'theme_location' => 'header-not-front',
+										'menu_class'     => 'primary-menu',
+									)
+								);
+							endif;
+							?>
+						</div>
+						<div class="col-md-6 footer-menu menu-middle">
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'footer',
+									'menu_class'     => 'secondary-menu',
+								)
+							);
+							?>	
+						</div>
+						<div class="col-md-4 footer-menu menu-right">
+							<a href="tel:+4915114217370">
+								<i class="fa fa-phone"></i>
+								+49 151 14217370
+							</a>
+							<a href="mailto:anita.staab@petticap.com">
+								<i class="fa fa-envelope"></i>
+								anita.staab@petticap.com
+							</a>
+							<br>
+							<a href="https://www.instagram.com/petticap/" target="_blank" rel="noopener noreferrer">
+								<i class="fab fa-instagram"></i>
+								PettiCap on Instagram
+							</a>
+							<a href="https://www.facebook.com/petticap.de" target="_blank" rel="noopener noreferrer">
+								<i class="fab fa-facebook-square"></i>
+								PettiCap on Facebook
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		<div class="site-info">
+			<div class="container">		
+				<i class="fa fa-copyright"></i>
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( ' 2020 by PettiCap', 'theme-gold' ), '' );
+				?>
+			</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/theme-gold/header.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Theme_Gold
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'theme-gold'); ?></a>
+		<header class="site-header header-big">
+			<nav id="site-navigation" class="main-navigation">
+				<div class="container">
+					<div class="row">
+						<div class="col col-5 col-sm-3 col-md-3">
+							<div class="site-title">
+								<?php
+								the_custom_logo();
+								?>
+							</div>
+						</div>
+						<div class="col col-7 col-sm-9 col-md-9 menu-row">
+							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" title="<?php esc_html_e('Toggle Menu', 'theme-gold'); ?>">
+								<div class="icon">
+									<div class="line"></div>
+									<div class="line"></div>
+									<div class="line"></div>
+								</label>
+							</button>
+							<div class="menu-wrapper">
+								<?php
+								if (is_front_page()) :
+									wp_nav_menu(
+										array(
+											'theme_location' => 'header',
+											'menu_class'     => 'primary-menu',
+										)
+									);
+								else :
+									wp_nav_menu(
+										array(
+											'theme_location' => 'header-not-front',
+											'menu_class'     => 'primary-menu',
+										)
+									);
+								endif;
+								?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</header>
