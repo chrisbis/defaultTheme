351d0fb8ec953c8dac1bcaa8534ee5a238f0d89c
Christoph <c.kleber@aol.com>
2020-07-30 16:09:51 +0200

    site-main main

--- wp-content/themes/theme-gold/page.php
+++ wp-content/themes/theme-gold/page.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main main">
+	<main id="primary" class="site-main main header-big">
 		<?php if (!is_page('datenschutz')) : ?>
 			<div id="cookie-layer">
 				<div class="text">
