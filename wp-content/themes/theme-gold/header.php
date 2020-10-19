0f086ef0571b66193bd8812c4b8eaf2e772ad4a1
Christoph <c.kleber@aol.com>
2020-10-19 17:54:29 +0200

    changes

--- wp-content/themes/theme-gold/header.php
+++ wp-content/themes/theme-gold/header.php
@@ -31,7 +31,7 @@
 			<nav id="site-navigation" class="main-navigation">
 				<div class="container">
 					<div class="row">
-					<div class="col col-4 col-sm-9 menu-row">
+					<div class="col col-6 menu-row">
 							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" title="<?php esc_html_e('Toggle Menu', 'theme-gold'); ?>">
 								<div class="icon">
 									<div class="line"></div>
@@ -59,7 +59,7 @@
 								?>
 							</div>
 						</div>
-						<div class="col col-8 col-sm-3">
+						<div class="col col-5">
 							<div class="site-title">
 								<a href="<?php echo esc_url( home_url( '/' ) ); ?>">k.</a>
 							</div>
